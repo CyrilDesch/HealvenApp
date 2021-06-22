d21ca245dd9b2ffb289f0bed5d0af19295eb6b57
Cyril Deschamps <77438408+CyrilDlaPompote@users.noreply.github.com>
2021-06-22 15:36:59 +0200

    coorect

--- src/navigationRef.js
+++ src/navigationRef.js
@@ -1,9 +1,7 @@
-import { createNavigationContainerRef } from '@react-navigation/native';
+import * as React from 'react';
 
-export const navigationRef = createNavigationContainerRef();
+export const navigationRef = React.createRef();
 
 export function navigate(name, params) {
-  if (navigationRef.isReady()) {
-    navigationRef.navigate(name, params);
-  }
+  navigationRef.current?.navigate(name, params);
 }
